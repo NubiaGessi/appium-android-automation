7d3df127d4215ecdc01d6065f2c0bb74a32f0bbf
NubiaGessi <bia.gessi@gmail.com>
2025-01-23 16:46:28 -0300

    update loginscreen

--- test/specs/login.e2e.js
+++ test/specs/login.e2e.js
@@ -3,7 +3,6 @@ import { expect } from '@wdio/globals'
 import { $ } from '@wdio/globals'
 import MenuScreen from '../screen/menu.screen.js'
 import LoginScreen from '../screen/login.screen.js'
-import loginScreen from '../screen/login.screen.js'
 
 describe('My Login application', () => {
     it('should login with invalid credentials', async () => {
